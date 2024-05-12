0681884cb327fc684761a676035783e69bba4f65
Deeptha Karthick <karthickdeeptha@gmail.com>
2024-05-12 23:04:55 +0530

    initial commit

---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+// import {useRouteError} from 'r'
+
+// const Error = () => {
+//     const error = useRouteError();
+//     if(error.status === 404) {
+//         return
+//     }
+// //   return <div>Error</div>;
+// };
+
+// // export default Error;
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,5 @@
+const Landing = () => {
+  return <div>HomeLayout</div>;
+};
+
+export default Landing;
